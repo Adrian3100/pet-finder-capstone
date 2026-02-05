6b0af4429c2cc54224483fa085d1732ff99cfb82
Adrian3100 <adrian.warfield3131@gmail.com>
2026-02-05 13:37:38 -0600

    Added code.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,37 @@
+import { useState } from "react";
 import PetsPage from "./pages/PetsPage.jsx";
-
+import MainPage from "./pages/MainPage.jsx";
+import LogInPage from "./pages/LogInPage.jsx";
+import "./App.css";
 export default function App() {
-  return <PetsPage />;
+  const [page,setPage] = useState("Main")
+   function GoToPets() {
+     setPage('Pets');
+   }
+   function GoToMain() {
+    setPage('Main');
+   }
+   function GoToLogIn() {
+    setPage('LogIn');
+   }
+  let content = <MainPage setPage={setPage}/>
+   if (page===('Main')) {
+    content = <MainPage setPage={setPage}/>
+   } else if (page===('Pets')) {
+    content = <PetsPage setPage={setPage}/>
+   }
+   else if (page==='LogIn') {
+    content = <LogInPage setPage={setPage}/>
+   }
+  return <div>
+    <h1>
+        Pets
+    </h1>
+    <nav>
+        <button onClick={GoToPets}>Pets</button>
+        <button onClick={GoToMain}>Main</button>
+        <button onClick={GoToLogIn}>LogIn</button>
+    </nav>
+    {content}
+    </div>;
 }
